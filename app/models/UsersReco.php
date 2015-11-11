89842a5b62226b40b06484251d6254c399127239
Gustavo Adolfo Mesa Roldán <gs.gore@gmail.com>
2015-11-11 21:28:32 +0000

    Sobic 1.2

--- app/models/UsersReco.php
+++ app/models/UsersReco.php
@@ -7,10 +7,8 @@
 class UsersRec extends \Illuminate\Database\Eloquent\Model
 {
 	protected $table = "usersReco";
-	protected $fillable = ['user_id','email','pass','key'];
+	protected $fillable = ['user_id','email','key_token'];
 	
-	protected $hidden = array('pass');
-
 	public function userReco() {
 		return $this->hasMany('User');
 	}
