2fb3b48fb376d40f24b6a0e675b4080d58133a86
drg809 <drg809@gmail.com>
2015-12-24 01:11:35 +0100

    Update call to Slim object in index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require __DIR__.'/../vendor/autoload.php';
 
 /** Creamos la api res y cargamos las llamadas **/
-$app = new \Slim\Slim();
+$app = new \Slim\App;
 
 /** Requerimos las rutas **/
 require '../app/main.php';
